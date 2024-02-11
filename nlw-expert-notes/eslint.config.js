7cee4f5aac31898a2a364b17a3136a531c27cdbc
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-02-11 11:49:30 -0300

    correção da configuração tailwind prettier eslint

--- nlw-expert-notes/eslint.config.js
+++ nlw-expert-notes/eslint.config.js
@@ -2,7 +2,7 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [{
-    plugin: { typescriptEslint },
+    plugins: { typescriptEslint },
     rules: {
         indent: "error"
         }
